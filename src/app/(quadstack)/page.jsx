a7c4ba1e1c6e1438f1e90279f71d543d9c7a2cf7
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-11-05 17:39:40 +0530

    Initialized scrolling text

--- src/app/(quadstack)/page.jsx
+++ src/app/(quadstack)/page.jsx
@@ -1,16 +1,18 @@
 import Hero from "@/components/client/Hero";
 import { Navbar } from "@/components/client/Navbar";
+import { ScrollText } from "@/components/client/ScrollText";
 
 export default function Home() {
   return (
     <>
       <div
         style={{ backgroundImage: "url('/bg.jpg')" }}
-        className="min-h-[300vh] w-full bg-no-repeat bg-cover"
+        className="min-h-[300vh] w-full bg-no-repeat bg-cover overflow-hidden"
       >
         <div>
           <Navbar />
           <Hero />
+          <ScrollText />
         </div>
       </div>
     </>
