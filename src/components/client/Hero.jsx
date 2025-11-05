dea6684f18383c768646af21fd6cf4de739f1864
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-11-05 19:02:06 +0530

    Minor ui changes

--- src/components/client/Hero.jsx
+++ src/components/client/Hero.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
 
 const Hero = () => {
   return (
-    <div className="min-h-[90vh] flex items-center justify-center ">
+    <div className="min-h-[90vh] flex items-center justify-center px-4 md:px-20">
       <div className="">
         <div className="max-w-3xl mx-auto text-center">
-          <h2 className="text-7xl font-bold text-background">
+          <div className="z-10 flex py-10 items-center justify-center">
+            <div
+              className={cn(
+                "group rounded-full border border-background/30 bg-foreground text-base text-background transition-all ease-in hover:cursor-pointer hover:bg-foreground/60"
+              )}
+            >
+              <div className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-background/60 hover:duration-300">
+                <span className="text-sm">Powered by QuadStack</span>
+              </div>
+            </div>
+          </div>
+          <h2 className="md:text-7xl sm:text-5xl text-4xl font-semibold text-background">
             Radically better observability{" "}
-            <span className="text-pink-400">stack</span>
+            <span className="text-violet-400">stack</span>
           </h2>
-          <p className="text-base mt-4 text-background/50">
-            Quadstack offers digital services including SEO, web apps, e-commerce website, and
-            automation.
+          <p className="max-w-lg mx-auto text-base mt-4 text-background/50">
+            Quadstack offers digital services including SEO, web apps,
+            e-commerce website, and automation.
           </p>
           <div className="mt-6">
             <Button variant={"outline"} size={"lg"} className={"rounded-3xl"}>
--- src/components/client/Navbar.jsx
+++ src/components/client/Navbar.jsx
@@ -5,9 +5,9 @@ import { Button } from "../ui/button";
 
 export function Navbar({ className }) {
   return (
-    <header className="fixed z-50 top-5 inset-x-0 flex justify-between items-center px-20">
+    <header className="fixed z-50 top-5 inset-x-0 flex justify-between items-center md:px-20 px-4">
       <div>
-        <h2 className="text-3xl font-bold text-background">QuadStack</h2>
+        <h2 className="text-3xl font-bold text-background">Quadstack</h2>
       </div>
       <div className={cn("max-w-lg", className)}>
         <div className="relative rounded-full border border-transparent dark:bg-background dark:border-foreground bg-linear-to-r from-foreground to-foreground shadow-input px-8 py-3">
