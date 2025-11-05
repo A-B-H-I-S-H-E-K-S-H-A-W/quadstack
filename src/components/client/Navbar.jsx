d16032e188385a7751cd59f25551406cd9ebe40f
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-11-05 17:41:18 +0530

    Minor ui changes

--- src/components/client/Navbar.jsx
+++ src/components/client/Navbar.jsx
@@ -5,11 +5,11 @@ import { Button } from "../ui/button";
 
 export function Navbar({ className }) {
   return (
-    <header className="fixed top-5 inset-x-0 flex justify-between items-center px-20">
+    <header className="fixed z-50 top-5 inset-x-0 flex justify-between items-center px-20">
       <div>
         <h2 className="text-3xl font-bold text-background">QuadStack</h2>
       </div>
-      <div className={cn("max-w-lg z-50", className)}>
+      <div className={cn("max-w-lg", className)}>
         <div className="relative rounded-full border border-transparent dark:bg-background dark:border-foreground bg-linear-to-r from-foreground to-foreground shadow-input px-8 py-3">
           <ul className="flex justify-center space-x-8 text-background">
             <li>Services</li>
